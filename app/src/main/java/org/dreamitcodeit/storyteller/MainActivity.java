97412559e41f6447d2eedbbbbb576858b172734e
Coleman Johnston <colemanmav@fb.com>
2017-07-12 14:44:21 -0700

    Created an activity that displays a simple map

--- app/src/main/java/org/dreamitcodeit/storyteller/MainActivity.java
+++ app/src/main/java/org/dreamitcodeit/storyteller/MainActivity.java
@@ -1,7 +1,8 @@
 package org.dreamitcodeit.storyteller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this,MapActivity.class));//TODO: don't do this
     }
 }
