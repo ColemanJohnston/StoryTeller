2cf8cbf829876f6b38fef03852b8b8cb94322f35
Maria De Angelis <mariadeangelis@fb.com>
2017-07-12 13:14:57 -0700

    Cleaned up

--- app/src/main/java/org/dreamitcodeit/storyteller/MapActivity.java
+++ app/src/main/java/org/dreamitcodeit/storyteller/MapActivity.java
@@ -2,6 +2,7 @@ package org.dreamitcodeit.storyteller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        String email = getIntent().getExtras().getString("email");
+
+        TextView tvWelcome = (TextView) findViewById(R.id.tvWelcome);
+        tvWelcome.setText("Welcome user " + email);
     }
 }
