c256ab2db066cea235bc20fddb5862ab8e8ac976
Maria De Angelis <mariadeangelis@fb.com>
2017-07-16 20:08:17 -0700

    In the process of converting author fragment to an activity

---
+++ app/src/main/java/org/dreamitcodeit/storyteller/AuthorActivity.java
@@ -0,0 +1,13 @@
+package org.dreamitcodeit.storyteller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_author);
+    }
+}
