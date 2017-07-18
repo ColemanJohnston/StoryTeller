0859dd961ee41670587544c58ee17fa7f43492f8
Maria De Angelis <mariadeangelis@fb.com>
2017-07-18 10:29:29 -0700

    Still crashing but have made progress

---
+++ app/src/main/java/org/dreamitcodeit/storyteller/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package org.dreamitcodeit.storyteller;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by mariadeangelis on 7/18/17.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
