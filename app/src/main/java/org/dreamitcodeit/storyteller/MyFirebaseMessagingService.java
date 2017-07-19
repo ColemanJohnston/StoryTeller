f22b62ade22dd4371a57d086763250af8b060820
Maria De Angelis <mariadeangelis@fb.com>
2017-07-19 14:46:13 -0700

    fixed bug but still not recieving message

--- app/src/main/java/org/dreamitcodeit/storyteller/MyFirebaseMessagingService.java
+++ app/src/main/java/org/dreamitcodeit/storyteller/MyFirebaseMessagingService.java
@@ -1,24 +1,24 @@
-//package org.dreamitcodeit.storyteller;
-//
-//import android.util.Log;
-//
-//import com.google.firebase.messaging.FirebaseMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-//
-///**
-// * Created by mariadeangelis on 7/19/17.
-// */
-//
-//public class MyFirebaseMessagingService extends FirebaseMessagingService {
-//
-//    private static final String TAG = "FCM Service";
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        // TODO: Handle FCM messages here.
-//        // If the application is in the foreground handle both data and notification messages here.
-//        // Also if you intend on generating your own notifications as a result of a received FCM
-//        // message, here is where that should be initiated.
-//        Log.d(TAG, "From: " + remoteMessage.getFrom());
-//        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-//    }
-//}
+package org.dreamitcodeit.storyteller;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by mariadeangelis on 7/19/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
