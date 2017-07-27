3f5846f8eb439fb0de0d0e22de02dedf32c4ae9f
Maria De Angelis <mariadeangelis@fb.com>
2017-07-27 10:50:22 -0700

    camera images are now saved into local storage. Not yet being recieved correctly from the intent. I also need to convert the uri that will be eventually returned to a bitmap so we can compress it and plop it into firebase

--- app/src/main/java/org/dreamitcodeit/storyteller/AuthorActivity.java
+++ app/src/main/java/org/dreamitcodeit/storyteller/AuthorActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -42,10 +43,12 @@ import org.dreamitcodeit.storyteller.models.User;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 //import static org.dreamitcodeit.storyteller.R.id.editor;
@@ -67,7 +70,6 @@ public class AuthorActivity extends AppCompatActivity {
     private Button btImportPhoto;
     private ImageView ivPreview;
     private String title;
-   // private RichEditor mEditor;
 
     private RadioButton rPersonal;
     private RadioButton rHistorical;
@@ -80,6 +82,7 @@ public class AuthorActivity extends AppCompatActivity {
 
     // for taking photos
     public Uri file;
+    String mCurrentPhotoPath;
     InputStream inputStream;
     Bitmap bity;
     private DatePicker dpCompose;
@@ -97,10 +100,6 @@ public class AuthorActivity extends AppCompatActivity {
 
         Firebase.setAndroidContext(this);
 
-        /*mAuth = FirebaseAuth.getInstance();
-        currentUser = mAuth.getCurrentUser();
-        userName = currentUser.getEmail();*/
-
         btSave = (Button) findViewById(R.id.btSave);
         etStoryBody = (EditText) findViewById(R.id.etStoryBody);
         etTitle = (EditText) findViewById(R.id.etTitle);
@@ -112,26 +111,6 @@ public class AuthorActivity extends AppCompatActivity {
         rPersonal = (RadioButton) findViewById(R.id.rPersonal);
         rHistorical = (RadioButton) findViewById(R.id.rHistorical);
         rFictional = (RadioButton) findViewById(R.id.rFictional);
-      //  mEditor = (RichEditor) findViewById(editor);
-
-        //mEditor.setEditorHeight(100);
-        //mEditor.setEditorFontSize(22);
-        //mEditor.setEditorFontColor(R.color.colorPrimaryDark);
-        //mEditor.setEditorBackgroundColor(Color.BLUE);
-        //mEditor.setBackgroundColor(Color.BLUE);
-        //mEditor.setBackgroundResource(R.drawable.bg);
-       // mEditor.setPadding(10, 10, 10, 10);
-        //mEditor.setBackground("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg");
-       // mEditor.setPlaceholder("Insert text here...");
-       // mEditor.setBold();
-
-      /*  findViewById(R.id.bold).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                mEditor.setBold();
-            }
-        });*/
-
-        //mEditor.setInputEnabled(false);
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("M/dd/yyyy");
         String today = dateFormat.format(Calendar.getInstance().getTime());
@@ -314,13 +293,18 @@ public class AuthorActivity extends AppCompatActivity {
         // For taking a photo in app
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK)
         {
+
             // Now we have the image from the camera
             Bundle extras = data.getExtras();
             Bitmap cameraImage = (Bitmap) extras.get("data");
 
-            bity = cameraImage;
+            Uri qualityImageUri = (Uri) extras.get( MediaStore.EXTRA_OUTPUT);
 
-            ivPreview.setImageBitmap(cameraImage);
+            ivPreview.setImageURI(qualityImageUri);
+
+//            bity = cameraImage;
+//
+//            ivPreview.setImageBitmap(cameraImage);
 
             // Now store the image in Firebase Cloud Storage
             storeImageCloud();
@@ -329,8 +313,47 @@ public class AuthorActivity extends AppCompatActivity {
 
     public void takePhoto()
     {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        // out with the old
+//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+        // in with the new lol
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "org.dreamitcodeit.storyteller.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     // Stores image in our Firebase Cloud Storage, backed by Google. Thanks Google!
@@ -368,8 +391,6 @@ public class AuthorActivity extends AppCompatActivity {
                 Uri downloadUrl = taskSnapshot.getDownloadUrl();
             }
         });
-
-
     }
 
     private void addToUserStoriesList(final String newStoryId){
